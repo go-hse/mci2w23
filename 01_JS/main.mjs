84739c29609b0472f3631afa431e56efbf23821b
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2023-10-05 10:26:36 +0200

    array functions

--- 01_JS/main.mjs
+++ 01_JS/main.mjs
@@ -3,24 +3,57 @@ import * as funktionModul from "./js/funcs.mjs";
 funktionModul.test();
 
 
-let o = {
-    key: 12,
-    "key;asdf": 12,
-}
-
 function onload() {
-    console.log("On load", new Date());
-    console.log(`das datum ist ${new Date()}`);
-    test();
-
-    if (a > 12) {
-        let b = a;
-        console.log(a, b);
-    } // b verschwindet
-
-    console.log(o.key, o["key;asdf"]);
-
 }
 
-window.onload = onload;
+window.onload = () => {
+    let arr = [1, 2, 3, 4, 5];
+    console.log(typeof arr, Array.isArray(arr), arr.length);
+    arr.push(5);
+
+    console.log(arr.pop(), arr.shift()); // letztes oder erstes Element entfernen
+
+
+    arr.splice(2, 1); // Element löschen
+    console.log(arr);
+
+    const i = 3;
+    if (arr.includes(i)) {
+        const idx = arr.indexOf(i);
+        const del = arr.splice(idx, 1);
+        console.log(arr, del);
+    }
+
+    for (let i = 0; i < arr.length; ++i) {
+        console.log(i, arr[i]);
+    }
+
+    for (let v of arr) {
+        console.log(v);
+    }
+
+    function toMap(v) {
+        return v * 2
+    };
+
+    arr = arr.map(toMap);
+    console.log(arr);
+
+
+    let objArr = [{ a: "Test", n: 15 }, { a: "Test 2", n: 16 }, { a: "Test 2", n: 20 }]
+
+    // let result = objArr.forEach(v => v.a += " B");
+    // console.log(result, objArr);
+
+    let filtered = objArr.filter(v => v.n > 15);
+    console.log(filtered, objArr);
+
+    let obj = { Vorname: "Max", Nachname: "Mustermann", Alter: 15 };
+    for (let att in obj) {
+        console.log(att, obj[att]);
+    }
+
+
+
+};
 
