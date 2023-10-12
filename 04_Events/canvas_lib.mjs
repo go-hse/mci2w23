c8554f8cbbd48e317e94980431e773d67cb76779
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2023-10-12 12:08:46 +0200

    circles mit Mouse

--- 04_Events/canvas_lib.mjs
+++ 04_Events/canvas_lib.mjs
@@ -41,6 +41,13 @@ export function circle(ctx, x, y, radius, fillStyle = "#fff", strokeStyle = "#00
     ctx.stroke();
 }
 
+
+export function distance(x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
 export function initCanvas(id) {
     const canvas = document.getElementById(id);
     const ctx = canvas.getContext('2d');
--- 04_Events/main.js
+++ 04_Events/main.js
@@ -2,7 +2,6 @@ import * as cl from "./canvas_lib.mjs";
 
 window.onload = () => {
     const ctx = cl.initCanvas('canvas');
-    const startTime = new Date();
 
     let mouseX = 0, mouseY = 0;
     window.addEventListener('mousemove', function (event) {
@@ -10,19 +9,27 @@ window.onload = () => {
         mouseY = event.clientY;
     });
 
-    const size = 40;
+    const size = 60;
+    const radius = 30;
+    const circles = [];
+    const offset = 40;
+    for (let x = 0; x < 10; ++x) {
+        for (let y = 0; y < 10; ++y) {
+            circles.push({ x, y });
+        }
+    }
+
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        const now = new Date();
-        const deltaTime = (now - startTime) / 1000; // Zeit ab Start in Sekunden
-        ctx.translate(mouseX, mouseY);
-        cl.cross(ctx, 0, 0);
-        ctx.rotate(deltaTime);
-        cl.rect(ctx, 0, 0, size, 2 * size, "#ff0");
-        cl.circle(ctx, 0, 0, 30, "#f00");
-        ctx.resetTransform();
-
+        for (let c of circles) {
+            const x = offset + c.x * size;
+            const y = offset + c.y * size;
+            const d = cl.distance(x, y, mouseX, mouseY);
+            const color = d < radius ? "#fff" : "#0af";
+            cl.circle(ctx, x, y, radius, color);
+        }
+        cl.cross(ctx, mouseX, mouseY);
         window.requestAnimationFrame(draw);
     }
 
