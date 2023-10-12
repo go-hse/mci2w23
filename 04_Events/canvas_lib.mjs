602bd1fc55a79343ecefeca052c374ba77846d45
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2023-10-12 11:29:09 +0200

    Initiale Version fuer Events

---
+++ 04_Events/canvas_lib.mjs
@@ -0,0 +1,56 @@
+export function rect(ctx, x, y, w, h, fillStyle = "#fff", strokeStyle = "#000", lineWidth = 1) {
+    ctx.fillStyle = fillStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillRect(x, y, w, h);
+    ctx.strokeRect(x, y, w, h);
+}
+
+export function line(ctx, x1, y1, x2, y2, strokeStyle = "#000", lineWidth = 1) {
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+export function cross(ctx, x, y, size = 10, strokeStyle = "#000", lineWidth = 1) {
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    ctx.moveTo(x - size, y);
+    ctx.lineTo(x + size, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y - size);
+    ctx.lineTo(x, y + size);
+    ctx.stroke();
+}
+
+export function circle(ctx, x, y, radius, fillStyle = "#fff", strokeStyle = "#000", lineWidth = 1) {
+    ctx.fillStyle = fillStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle, true);
+    ctx.fill();
+    ctx.stroke();
+}
+
+
+
+export function initCanvas(id) {
+    const canvas = document.getElementById(id);
+    const ctx = canvas.getContext('2d');
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
+
+    return ctx;
+}
---
+++ 04_Events/main.js
@@ -0,0 +1,32 @@
+import * as cl from "./canvas_lib.mjs";
+
+window.onload = () => {
+    const ctx = cl.initCanvas('canvas');
+    const startTime = new Date();
+
+    let mouseX = 0, mouseY = 0;
+    window.addEventListener('mousemove', function (event) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    });
+
+    const size = 40;
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const now = new Date();
+        const deltaTime = (now - startTime) / 1000; // Zeit ab Start in Sekunden
+        {
+            ctx.translate(mouseX, mouseY);
+            cl.cross(ctx, 0, 0);
+            ctx.rotate(deltaTime);
+            cl.rect(ctx, 0, 0, size, 2 * size, "#ff0");
+            ctx.resetTransform();
+        }
+
+        window.requestAnimationFrame(draw);
+    }
+
+    draw();
+};
+
