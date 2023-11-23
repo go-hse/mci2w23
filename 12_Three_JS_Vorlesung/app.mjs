b5b081c19f2bb0c3586ea9ee7c1207e25d88aa36
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2023-11-23 10:40:08 +0100

    Demo mit Objekt-Feld und Schatten

--- 12_Three_JS_Vorlesung/app.mjs
+++ 12_Three_JS_Vorlesung/app.mjs
@@ -5,10 +5,10 @@ console.log("ThreeJs " + THREE.REVISION);
 window.onload = function () {
     let scene = new THREE.Scene();
 
-
     scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
     let light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0, 6, 0);
+    light.castShadow = true;
     scene.add(light);
 
     let camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 100);
@@ -16,29 +16,70 @@ window.onload = function () {
     camera.position.set(0, 0, 1);
     scene.add(camera);
 
+    const geometries = [
+        new THREE.BoxGeometry(0.25, 0.25, 0.25),
+        new THREE.ConeGeometry(0.1, 0.4, 64),
+        new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
+        new THREE.IcosahedronGeometry(0.2, 3),
+        new THREE.PlaneGeometry(1, 1),
+        new THREE.TorusKnotGeometry(.2, .03, 50, 16),
+        new THREE.TorusGeometry(0.2, 0.04, 64, 32)
+    ];
 
-    let box = new THREE.Mesh(
-        new THREE.BoxGeometry(0.2, 0.2, 0.2),
-        new THREE.MeshStandardMaterial({
+    function randomMaterial() {
+        return new THREE.MeshStandardMaterial({
             color: Math.random() * 0xff3333,
             roughness: 0.7,
-            metalness: 0.0,
-        }));
-    scene.add(box);
+            metalness: 0.0
+        });
+    }
 
-    box.position.z = -1;
+    function add(i, parent, x = 0, y = 0, z = 0) {
+        const object = new THREE.Mesh(geometries[i], randomMaterial());
+        object.position.set(x, y, z);
+        object.castShadow = true;
+        parent.add(object);
+        return object;
+    }
+
+
+    let array = [];
+    const delta = 0.4, z = -4;
+
+    let counter = 0;
+    for (let x = -1; x < 1; x += delta) {
+        for (let y = -1; y < 1; y += delta) {
+            const idx = (++counter) % geometries.length;
+            array.push(add(idx, scene, x, y, z));
+
+        }
+    }
+
+    const groundGeo = new THREE.PlaneGeometry(20, 20, 64);
+    const groundMat = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+    const ground = new THREE.Mesh(groundGeo, groundMat);
+    ground.position.set(0, -1, -4);
+    ground.receiveShadow = true;
+
+    scene.add(ground);
+    ground.rotation.x = -Math.PI / 2;
 
     let renderer = new THREE.WebGLRenderer({
         antialias: true,
     });
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
 
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
     function render() {
-        box.rotation.x += 0.04;
-        box.rotation.y += 0.01;
+        for (let o of array) {
+            o.rotation.x += 0.04;
+            o.rotation.z += 0.01;
+        }
         renderer.render(scene, camera);
     }
     renderer.setAnimationLoop(render);
